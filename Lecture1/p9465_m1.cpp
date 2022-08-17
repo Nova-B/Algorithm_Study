cdce32fb74e9b86ac7853366479780e87e08a846
Nova-B <seongbeom1115@gmail.com>
2022-08-18 08:45:54 +0900

    Create p9465_m1.cpp

---
+++ Lecture1/p9465_m1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int a[2][1000000];
+int main()
+{
+	int t;
+	cin >> t;
+	int index = -1;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int res = 0;
+		for (int i = 0; i <2; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				cin >> a[i][j];
+			}
+		}
+		for (int j = 1; j < n; j++)
+		{
+			res +=
+		}
+	}
+	return 0;
+}
